8145095604395fc167b407b61279e493b115e6ac
Arthus Frappé <32646968+arthsf@users.noreply.github.com>
2023-11-29 10:38:57 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Configure pending installations
+sudo dpkg --configure -a
+
+# Update package list
+sudo apt-get update -y
+
+# Install dependencies
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add Docker repository
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo $
+# Update package list again
+sudo apt-get update -y
